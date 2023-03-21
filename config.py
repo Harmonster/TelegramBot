91ae3cf431e48b53872d63e77d505f3777bf0740
Harmonster <dennyharmless@gmail.com>
2023-03-21 23:11:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ host="37.77.105.162"
 user="Tarabotti"
 password="gGJ130T90!"
 database="Diplom"
-api_token="6205532395:AAFK4YZK31d3T6ZhfxMxnI7m4NPuTJHth7M"
+api_token=""
 
 
 #query = "INSERT INTO `Diplom`.`staff` (`name_staff`, `email_staff`, `phone_staff`, `telegram_id`) VALUES (%s, %s, %s, %s);"
